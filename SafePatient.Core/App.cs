0bac6ff6ab6abfb3c607efdc5dc14858f91063f0
Lihle Mdikili <Ltmdikili@gmail.com>
2018-10-25 14:37:48 +0200

    Chnaged project to MvvmCross

---
+++ SafePatient.Core/App.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MvvmCross;
+using MvvmCross.Views;
+using MvvmCross.ViewModels;
+
+namespace SafePatient.Core
+{
+    public class App: MvxApplication
+    {
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+        }
+    }
+}
